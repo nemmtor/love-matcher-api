8c1c88275f32ace4d796de4a7ba6a0181cde1693
nemmtor <kacper0witas@gmail.com>
2020-07-17 14:45:22 +0200

    Add few things to eslint

--- src/handlers.ts
+++ src/handlers.ts
@@ -8,14 +8,13 @@ type HelloBuilder = (name: string) => HelloResponse;
 
 const helloBuilder: HelloBuilder = (name) => ({ hello: name });
 
-export const rootHandler = (_req: Request, res: Response) => {
+export const rootHandler = (_req: Request, res: Response): Response => {
     return res.send('API is working 🤓');
 };
 
-export const helloHandler = (req: Request, res: Response) => {
+export const helloHandler = (req: Request, res: Response): Response => {
     const { params } = req;
     const { name = 'World' } = params;
     const response = helloBuilder(name);
-
     return res.json(response);
 };
