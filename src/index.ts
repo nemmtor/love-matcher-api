b199cad0b64d8a94051601d9e964ecbebc2af09a
nemmtor <kacper0witas@gmail.com>
2020-07-17 14:58:27 +0200

    Change all files to CLRF

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ const port = process.env.PORT || '8000';
 
 app.get('/', rootHandler);
 app.get('/hello/:name', helloHandler);
-var x = 5;
 app.listen(port, (err) => {
   if (err) return console.error(err);
   return console.log(`Server is listening on ${port}`);
