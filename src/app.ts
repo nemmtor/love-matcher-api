ff344580e361c0b3333032760ff236a03f0c8499
nemmtor <kacper0witas@gmail.com>
2020-07-22 14:44:35 +0200

    Adds ip limit and speed limit to whole API

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,29 @@
-import express from 'express';
 import cors from 'cors';
+import express from 'express';
+import rateLimit from 'express-rate-limit';
+import slowDown from 'express-slow-down';
 
 import routes from './routes';
 
+const speedLimiter = slowDown({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  delayAfter: 5, // allow 5 requests per 15 minutes, then...
+  delayMs: 1000, // begin adding 1000ms of delay per request above 100:
+  // request # 6 is delayed by  1000ms
+  // request # 7 is delayed by 2000ms
+  // request # 8 is delayed by 3000ms
+  // etc.
+});
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
 const app = express();
 app.use(cors());
+app.use(limiter);
+app.use(speedLimiter);
 
 // Routes
 app.use('/match', routes.match);
