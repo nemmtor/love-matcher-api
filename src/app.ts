2c810bac5cd4b1da4d9928907ed8201e55992834
nemmtor <kacper0witas@gmail.com>
2020-07-22 11:24:22 +0200

    Adds cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+
 import routes from './routes';
 
 const app = express();
+app.use(cors());
 
+// Routes
 app.use('/match', routes.match);
 
 export default app;
